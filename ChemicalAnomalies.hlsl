4bbf55dfc7a5fa3fb2fc17a71249306887ffe7e3
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-03-24 15:38:38 +0100

    Create ChemicalAnomalies.hlsl

---
+++ ChemicalAnomalies.hlsl
@@ -0,0 +1,10 @@
+//the first thing to do is to 
+//prevent merging systems. in this shader,
+//we czn do it by 2 sided brdf.
+//this mean that error checking is needed
+//but at micro level.
+
+float getMirrors(...)
+{}
+
+void checkErrorByDistribution...
