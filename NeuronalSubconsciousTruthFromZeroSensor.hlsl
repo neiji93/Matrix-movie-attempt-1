7d66dfff895a5989a11f4fbe93c5768de144c44d
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-03-28 16:40:56 +0100

    Create NeuronalSubconsciousTruthFromZeroSensor.hlsl

---
+++ NeuronalSubconsciousTruthFromZeroSensor.hlsl
@@ -0,0 +1,18 @@
+
+
+//we can adamt sensors tonretrieve...
+//can be used in cg.
+//then the zeros valies on models refer
+//to solid torus intellogence.
+//as i said it can be used in cg.
+floatngetSolidTorusRandomValueFromLeapMotionSensor(){}
+
+
+
+//using computer vision
+//the subconscious truth can be fou'd by
+//continuous personal forbidden values.
+//then the forbidden values are de young values 
+//because child can return to life for everybodh.
+//then it means that assertions may be all wrong.
+float getassertionOneTwo(){}  //in our porject, the third element is zn anomaly.
